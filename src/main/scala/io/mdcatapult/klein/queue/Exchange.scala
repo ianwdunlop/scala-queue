983bf40bdfd1c7d8bf8420414eab5d04a27297dc
Daniel Sheppard <daniel.sheppard@md.catapult.org.uk>
2020-02-24 11:50:46 +0000

    simplify get or else of option.

--- src/main/scala/io/mdcatapult/klein/queue/Exchange.scala
+++ src/main/scala/io/mdcatapult/klein/queue/Exchange.scala
@@ -24,7 +24,7 @@ case class Exchange[T <: Envelope](name: String, routingKey: Option[String] = No
     rabbit ! Message.exchange(
       envelope,
       name,
-      if (routingKey.isDefined) routingKey.get else "",
+      routingKey.getOrElse(""),
       properties)
 
 }
