f82cda1bd1c0e67c6bb25488c489e355e78f32db
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2020-10-21 11:30:33 +0100

    Optionally send errors to error queue as well as logging

--- src/main/scala/io/mdcatapult/klein/queue/RecoveryStrategy.scala
+++ src/main/scala/io/mdcatapult/klein/queue/RecoveryStrategy.scala
@@ -1,11 +1,16 @@
 package io.mdcatapult.klein.queue
 
+import java.time.LocalDateTime
+
 import com.rabbitmq.client.Channel
 import com.spingo.op_rabbit.Directives._
 import com.spingo.op_rabbit.RecoveryStrategy.{`x-original-exchange`, `x-original-routing-key`, `x-retry`}
-import com.spingo.op_rabbit.properties.{MessageProperty, PimpedBasicProperties}
+import com.spingo.op_rabbit.properties.{Header, MessageProperty, PimpedBasicProperties}
+import com.spingo.op_rabbit.properties.HeaderValue.{StringHeaderValue => Value}
 import com.spingo.op_rabbit.{Binding, Directives, Handler, properties, Exchange => OpExchange, Queue => OpQueue, RecoveryStrategy => OpRecoveryStrategy}
+import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.scalalogging.LazyLogging
+import io.mdcatapult.klein.queue.Converter.toStrings
 import org.slf4j.MDC
 
 class RecoveryStrategyWrap(fn: (String, Channel, Throwable) => Handler) extends OpRecoveryStrategy {
@@ -24,6 +29,8 @@ object RecoveryStrategy extends LazyLogging {
                   exchange: OpExchange[OpExchange.Direct.type] = OpExchange.default,
                   retryCount: Int = 3,
                 ): OpRecoveryStrategy = new OpRecoveryStrategy {
+
+    val config: Config = ConfigFactory.load()
 
     private val getRetryCount = property(`x-retry`) | Directives.provide(0)
 
@@ -52,7 +59,8 @@ object RecoveryStrategy extends LazyLogging {
       }
 
     /*
-    Re-queue the message if there are any retries available. Otherwise log the error.
+     * Re-queue the message if there are any retries available. Otherwise log the error.
+     * Optionally send the error to the errors queue if the error.queue config flag is true
      */
     def apply(queueName: String, channel: Channel, exception: Throwable): Handler =
       getRetryCount {
@@ -63,7 +71,22 @@ object RecoveryStrategy extends LazyLogging {
           body(Directives.as[String]) { message =>
             MDC.put("original-message", message)
             logger.error(s"Exhausted retries for queue $queueName", exception)
-            nack()
+            if (config.getBoolean("error.queue")) {
+              enqueue(
+                errorQueueName,
+                channel,
+                List(
+                  Header("x-consumer", Value(consumerName.getOrElse("undeclared"))),
+                  Header("x-queue", Value(queueName)),
+                  Header("x-datetime", Value(LocalDateTime.now().toString)),
+                  Header("x-exception", Value(exception.getClass.getCanonicalName)),
+                  Header("x-message", Value(exception.getMessage)),
+                  Header("x-stack-trace", Value(toStrings(exception).mkString("\n"))),
+                )
+              )
+            } else {
+              nack()
+            }
           }
       }
   }
