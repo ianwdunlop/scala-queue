a036461f2f613420bcd94c3a2a12472ffe96eebf
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2020-11-23 13:46:43 +0000

    Test message persistence property

--- src/it/scala/io/mdcatapult/klein/queue/QueueIntegrationSpec.scala
+++ src/it/scala/io/mdcatapult/klein/queue/QueueIntegrationSpec.scala
@@ -28,8 +28,7 @@ class QueueIntegrationSpec extends TestKit(ActorSystem("QueueIntegrationTest", C
   with Matchers
   with BeforeAndAfterAll with ScalaFutures with Eventually {
 
-  "A queue" should
-    "be created if it does not exist" in {
+  "Creating a queue with persist false" should "not persist messages" in {
 
     implicit val config: Config = ConfigFactory.load()
 
@@ -59,6 +58,40 @@ class QueueIntegrationSpec extends TestKit(ActorSystem("QueueIntegrationTest", C
     }
     channel.basicConsume(queueName, deliverCallback, cancelCallback)
     queue.send(Message("Don't persist me"))
+
+    eventually (timeout(Span(5, Seconds))) {deliveryMode.get() should be >= 1 }
+  }
+
+  "A queue" should "persists messages by default" in {
+
+    implicit val config: Config = ConfigFactory.load()
+
+    // Note that we need to include a topic if we want the queue to be created
+    val consumerName = Option(config.getString("op-rabbit.topic-exchange-name"))
+    val queueName = "persistent-test-queue"
+
+    val queue = Queue[Message](queueName, consumerName)
+
+    // Use the java rabbit libs. Makes getting the message properties much simpler
+    val factory = new ConnectionFactory
+    factory.setHost(config.getStringList("op-rabbit.connection.hosts").get(0))
+    factory.setUsername(config.getString("op-rabbit.connection.username"))
+    factory.setPassword(config.getString("op-rabbit.connection.password"))
+    factory.setVirtualHost(config.getString("op-rabbit.connection.virtual-host"))
+    val connection = factory.newConnection
+    val channel = connection.createChannel
+
+    val deliveryMode: AtomicInt = AtomicInt(0)
+    val cancelCallback: CancelCallback = (consumerTag) => {
+
+    }
+    val deliverCallback:DeliverCallback = (consumerTag: String, delivery: Delivery) => {
+      if (delivery.getProperties.getDeliveryMode == 2) {
+        deliveryMode.add(1)
+      }
+    }
+    channel.basicConsume(queueName, deliverCallback, cancelCallback)
+    queue.send(Message("Persist me"))
 
     eventually (timeout(Span(5, Seconds))) {deliveryMode.get() should be >= 1 }
   }
